63a86749ee30be97f800e1d18931206ec744d07d
Agustin Sackmann <asackmann@biapplications.com.ar>
2023-04-13 11:05:49 -0300

    cleanup

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -1,4 +1,5 @@
 import openai
+import time
 from config import Config
 cfg = Config()
 
@@ -6,11 +7,24 @@ openai.api_key = cfg.openai_api_key
 
 # Overly simple abstraction until we create something better
 def create_chat_completion(messages, model=None, temperature=None, max_tokens=None)->str:
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
+    # Make the request to the OpenAI API and handle potential errors
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+    except openai.error.APIError as e:
+        print(f"Encountered API error: {e}")
+        print("Retrying in 10 seconds...")
+        time.sleep(10)  # Wait for 10 seconds
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
 
     return response.choices[0].message["content"]
+
